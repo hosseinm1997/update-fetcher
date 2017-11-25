9b1deb6361c34cd3f6fb74822995ee7f7c6e1eec
hossein <hossein.moshfegh1997@gmail.com>
2017-11-25 04:33:01 +0330

    uploading project

---
+++ FetchBotUpdate/Form1.Designer.cs
@@ -0,0 +1,267 @@
+﻿namespace FetchBotUpdate
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.txtUrlAddress = new System.Windows.Forms.TextBox();
+            this.btnFetch = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnStopFetch = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtToken = new System.Windows.Forms.TextBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtInterval = new System.Windows.Forms.TextBox();
+            this.txtOffset = new System.Windows.Forms.TextBox();
+            this.notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.show = new System.Windows.Forms.ToolStripMenuItem();
+            this.hide = new System.Windows.Forms.ToolStripMenuItem();
+            this.timReconnect = new System.Windows.Forms.Timer(this.components);
+            this.lblStatus = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.contextMenuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // txtUrlAddress
+            // 
+            this.txtUrlAddress.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(178)));
+            this.txtUrlAddress.Location = new System.Drawing.Point(39, 175);
+            this.txtUrlAddress.Name = "txtUrlAddress";
+            this.txtUrlAddress.Size = new System.Drawing.Size(316, 26);
+            this.txtUrlAddress.TabIndex = 0;
+            this.txtUrlAddress.Text = "http://localhost/yourfile.php";
+            // 
+            // btnFetch
+            // 
+            this.btnFetch.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(178)));
+            this.btnFetch.Location = new System.Drawing.Point(406, 123);
+            this.btnFetch.Name = "btnFetch";
+            this.btnFetch.Size = new System.Drawing.Size(105, 48);
+            this.btnFetch.TabIndex = 1;
+            this.btnFetch.Text = "Start";
+            this.btnFetch.UseVisualStyleBackColor = true;
+            this.btnFetch.Click += new System.EventHandler(this.btnFetch_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(178)));
+            this.label1.Location = new System.Drawing.Point(39, 137);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(211, 20);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Url Address to send updates";
+            // 
+            // btnStopFetch
+            // 
+            this.btnStopFetch.Enabled = false;
+            this.btnStopFetch.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(178)));
+            this.btnStopFetch.Location = new System.Drawing.Point(406, 205);
+            this.btnStopFetch.Name = "btnStopFetch";
+            this.btnStopFetch.Size = new System.Drawing.Size(105, 48);
+            this.btnStopFetch.TabIndex = 3;
+            this.btnStopFetch.Text = "Stop";
+            this.btnStopFetch.UseVisualStyleBackColor = true;
+            this.btnStopFetch.Click += new System.EventHandler(this.btnStopFetch_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(178)));
+            this.label2.Location = new System.Drawing.Point(39, 37);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(82, 20);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Bot Token";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // txtToken
+            // 
+            this.txtToken.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(178)));
+            this.txtToken.Location = new System.Drawing.Point(39, 69);
+            this.txtToken.Name = "txtToken";
+            this.txtToken.Size = new System.Drawing.Size(316, 26);
+            this.txtToken.TabIndex = 4;
+            this.txtToken.Text = "Your bot token from @BotFather";
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(178)));
+            this.label3.Location = new System.Drawing.Point(39, 233);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(268, 20);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Fetch updates every   (in millisecond)";
+            // 
+            // txtInterval
+            // 
+            this.txtInterval.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(178)));
+            this.txtInterval.Location = new System.Drawing.Point(39, 271);
+            this.txtInterval.Name = "txtInterval";
+            this.txtInterval.Size = new System.Drawing.Size(316, 26);
+            this.txtInterval.TabIndex = 6;
+            this.txtInterval.Text = "500";
+            // 
+            // txtOffset
+            // 
+            this.txtOffset.Enabled = false;
+            this.txtOffset.Location = new System.Drawing.Point(403, 302);
+            this.txtOffset.Name = "txtOffset";
+            this.txtOffset.Size = new System.Drawing.Size(105, 20);
+            this.txtOffset.TabIndex = 8;
+            this.txtOffset.Text = "124749127";
+            // 
+            // notifyIcon1
+            // 
+            this.notifyIcon1.ContextMenuStrip = this.contextMenuStrip1;
+            this.notifyIcon1.Icon = ((System.Drawing.Icon)(resources.GetObject("notifyIcon1.Icon")));
+            this.notifyIcon1.Text = "Status: Not Started";
+            this.notifyIcon1.Visible = true;
+            this.notifyIcon1.DoubleClick += new System.EventHandler(this.notifyIcon1_DoubleClick);
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.show,
+            this.hide});
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(104, 48);
+            this.contextMenuStrip1.ItemClicked += new System.Windows.Forms.ToolStripItemClickedEventHandler(this.contextMenuStrip1_ItemClicked);
+            // 
+            // show
+            // 
+            this.show.Name = "show";
+            this.show.Size = new System.Drawing.Size(103, 22);
+            this.show.Text = "Show";
+            // 
+            // hide
+            // 
+            this.hide.AccessibleName = "";
+            this.hide.Name = "hide";
+            this.hide.Size = new System.Drawing.Size(103, 22);
+            this.hide.Text = "Hide";
+            // 
+            // timReconnect
+            // 
+            this.timReconnect.Interval = 1000;
+            this.timReconnect.Tick += new System.EventHandler(this.timReconnect_Tick);
+            // 
+            // lblStatus
+            // 
+            this.lblStatus.AutoEllipsis = true;
+            this.lblStatus.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(178)));
+            this.lblStatus.Location = new System.Drawing.Point(412, 52);
+            this.lblStatus.Name = "lblStatus";
+            this.lblStatus.Size = new System.Drawing.Size(99, 58);
+            this.lblStatus.TabIndex = 10;
+            this.lblStatus.Text = "Not Started";
+            this.lblStatus.TextChanged += new System.EventHandler(this.lblStatus_TextChanged);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(178)));
+            this.label4.Location = new System.Drawing.Point(412, 21);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(68, 19);
+            this.label4.TabIndex = 11;
+            this.label4.Text = "Status : ";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Tahoma", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(178)));
+            this.label5.Location = new System.Drawing.Point(403, 278);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(85, 14);
+            this.label5.TabIndex = 12;
+            this.label5.Text = "Last update id";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(546, 342);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.lblStatus);
+            this.Controls.Add(this.txtOffset);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtInterval);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtToken);
+            this.Controls.Add(this.btnStopFetch);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnFetch);
+            this.Controls.Add(this.txtUrlAddress);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Fetch Telegram Bot Updates";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.Resize += new System.EventHandler(this.Form1_Resize);
+            this.contextMenuStrip1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtUrlAddress;
+        private System.Windows.Forms.Button btnFetch;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnStopFetch;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtToken;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtInterval;
+        private System.Windows.Forms.TextBox txtOffset;
+        private System.Windows.Forms.NotifyIcon notifyIcon1;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem show;
+        private System.Windows.Forms.ToolStripMenuItem hide;
+        private System.Windows.Forms.Timer timReconnect;
+        private System.Windows.Forms.Label lblStatus;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+    }
+}
+
---
+++ FetchBotUpdate/Form1.cs
@@ -0,0 +1,252 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+using System.Collections.Specialized;
+using System.Net;
+using Newtonsoft.Json;
+using System.Web.Script.Serialization;
+using Microsoft.CSharp.RuntimeBinder;
+using Newtonsoft.Json.Linq;
+using System.IO;
+
+namespace FetchBotUpdate
+{
+    public partial class Form1 : Form
+    {
+        string setting_file_address = Application.StartupPath + "\\fetcher_setting.json";
+        bool hidden = false;
+        byte max_reconnect_time_seconds = 30;
+        byte reconnect_times=0;
+        bool fetching = false;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            using (WebClient client = new WebClient())
+            {
+                try
+                {
+
+                    string get_update_api = "https://api.telegram.org/bot" + txtToken.Text.Trim() + "/getUpdates";
+                    byte[] response =
+                    client.UploadValues(get_update_api, new NameValueCollection()
+                   {
+                       { "offset", (Convert.ToInt64(txtOffset.Text) + 1).ToString() },
+                       //{ "favorite+flavor", "flies" }
+                   });
+
+                    string result = System.Text.Encoding.UTF8.GetString(response);
+
+                    //dynamic updates = JObject.Parse("{\"ok\":true,\"result\":[{\"update_id\":124749113, \"message\":{\"message_id\":419,\"from\":{\"id\":203712431,\"is_bot\":false,\"first_name\":\"Ho\u00a7einM\",\"username\":\"hosseinm1997\",\"language_code\":\"en-US\"},\"chat\":{\"id\":203712431,\"first_name\":\"Ho\u00a7einM\",\"username\":\"hosseinm1997\",\"type\":\"private\"},\"date\":1510132178,\"text\":\"/start\",\"entities\":[{\"offset\":0,\"length\":6,\"type\":\"bot_command\"}]}},{\"update_id\":124749114, \"message\":{\"message_id\":420,\"from\":{\"id\":203712431,\"is_bot\":false,\"first_name\":\"Ho\u00a7einM\",\"username\":\"hosseinm1997\",\"language_code\":\"en-US\"},\"chat\":{\"id\":203712431,\"first_name\":\"Ho\u00a7einM\",\"username\":\"hosseinm1997\",\"type\":\"private\"},\"date\":1510138405,\"text\":\"/start\",\"entities\":[{\"offset\":0,\"length\":6,\"type\":\"bot_command\"}]}},{\"update_id\":124749115, \"message\":{\"message_id\":421,\"from\":{\"id\":203712431,\"is_bot\":false,\"first_name\":\"Ho\u00a7einM\",\"username\":\"hosseinm1997\",\"language_code\":\"en-US\"},\"chat\":{\"id\":203712431,\"first_name\":\"Ho\u00a7einM\",\"username\":\"hosseinm1997\",\"type\":\"private\"},\"date\":1510138405,\"text\":\"/start\",\"entities\":[{\"offset\":0,\"length\":6,\"type\":\"bot_command\"}]}},{\"update_id\":124749116, \"message\":{\"message_id\":422,\"from\":{\"id\":203712431,\"is_bot\":false,\"first_name\":\"Ho\u00a7einM\",\"username\":\"hosseinm1997\",\"language_code\":\"en-US\"},\"chat\":{\"id\":203712431,\"first_name\":\"Ho\u00a7einM\",\"username\":\"hosseinm1997\",\"type\":\"private\"},\"date\":1510138406,\"text\":\"/start\",\"entities\":[{\"offset\":0,\"length\":6,\"type\":\"bot_command\"}]}},{\"update_id\":124749117, \"message\":{\"message_id\":423,\"from\":{\"id\":203712431,\"is_bot\":false,\"first_name\":\"Ho\u00a7einM\",\"username\":\"hosseinm1997\",\"language_code\":\"en-US\"},\"chat\":{\"id\":203712431,\"first_name\":\"Ho\u00a7einM\",\"username\":\"hosseinm1997\",\"type\":\"private\"},\"date\":1510140995,\"text\":\"/start\",\"entities\":[{\"offset\":0,\"length\":6,\"type\":\"bot_command\"}]}},{\"update_id\":124749118, \"message\":{\"message_id\":424,\"from\":{\"id\":203712431,\"is_bot\":false,\"first_name\":\"Ho\u00a7einM\",\"username\":\"hosseinm1997\",\"language_code\":\"en-US\"},\"chat\":{\"id\":203712431,\"first_name\":\"Ho\u00a7einM\",\"username\":\"hosseinm1997\",\"type\":\"private\"},\"date\":1510140996,\"text\":\"/start\",\"entities\":[{\"offset\":0,\"length\":6,\"type\":\"bot_command\"}]}}]}");
+                    dynamic updates = JObject.Parse(result);
+                    if (updates["result"].Count > 0)
+                    {
+                        foreach (var item in updates["result"])
+                        {
+                            dynamic a = JsonConvert.SerializeObject(item);
+                            byte[] tresponse =
+                            client.UploadValues(txtUrlAddress.Text.Trim(), new NameValueCollection(){
+                            {"update" , a}
+                            //{ "offset", "Cosby" },
+                            //{ "favorite+flavor", "flies" }
+                        });
+                            //string tresult = System.Text.Encoding.UTF8.GetString(response);
+                        }
+
+                        txtOffset.Text = (string)updates["result"].Last["update_id"];
+                        saveSetting();
+                    }
+                    disableReconnectTimer();
+                    if(!timer1.Enabled)
+                        timer1.Enabled = true;
+
+                    if(!fetching)
+                        lblStatus.Text = "Fetching";
+
+                }
+                catch (WebException webexception)
+                {
+                    timer1.Enabled = false;
+                    enableReconnectTimer();
+                    reconnect_times++;
+                    if (reconnect_times == max_reconnect_time_seconds)
+                    {
+                        disableReconnectTimer();
+
+                        if (hidden)
+                        {
+                            this.Show();
+                            this.WindowState = FormWindowState.Normal;
+                            hidden = !hidden;
+                        }
+
+                        if (MessageBox.Show("There is no internet connection After " + max_reconnect_time_seconds.ToString() + " seconds trying. Check your network\nTry reconnect again?", "Connection failed", MessageBoxButtons.YesNo,MessageBoxIcon.Error) == System.Windows.Forms.DialogResult.Yes)
+                        {
+                            enableReconnectTimer();
+                        }
+                        else
+                        {
+                            btnStopFetch_Click(null, null);
+                        }
+                    }
+                }
+            }
+        }
+
+        private void enableReconnectTimer()
+        {
+            if (!timReconnect.Enabled)
+            {
+                reconnect_times = 0;
+                timReconnect.Enabled = true;
+                fetching = false;
+                lblStatus.Text = "Trying reconnecting...";
+            }
+        }
+
+        private void disableReconnectTimer()
+        {
+            if (timReconnect.Enabled)
+            {
+                timReconnect.Enabled = false;
+            }
+        }
+
+        private void btnFetch_Click(object sender, EventArgs e)
+        {
+            if (txtToken.Text.Trim() == "" || txtUrlAddress.Text.Trim() == "" || txtInterval.Text.Trim() == "")
+            {
+                MessageBox.Show("Please fill all text fields before start","Empty Fields",MessageBoxButtons.OK,MessageBoxIcon.Error);
+                return;
+            }
+
+            timer1.Interval = int.Parse(txtInterval.Text.Trim());
+            timer1.Enabled = true;
+
+            txtToken.Enabled = false;
+            txtUrlAddress.Enabled = false;
+            txtInterval.Enabled = false;
+            btnFetch.Enabled = false;
+            btnStopFetch.Enabled = true;
+
+            fetching = !fetching;
+            lblStatus.Text = "Fetching";
+        }
+
+        private void btnStopFetch_Click(object sender, EventArgs e)
+        {
+            timer1.Enabled = false;
+            timReconnect.Enabled = false;
+
+            txtToken.Enabled = true;
+            txtUrlAddress.Enabled = true;
+            txtInterval.Enabled = true;
+            btnFetch.Enabled = true;
+            btnStopFetch.Enabled = false;
+
+            fetching = !fetching;
+            lblStatus.Text = "Stopped";
+        }
+
+        private void saveSetting()
+        {
+            JObject o = new JObject();
+            o["last_token"] = txtToken.Text;
+            o["last_url"] = txtUrlAddress.Text;
+            o["last_id"] = txtOffset.Text;
+            o["last_interval"] = txtInterval.Text;
+            StreamWriter sw = new StreamWriter(setting_file_address);
+            sw.Write(o.ToString());
+            sw.Close();
+        }
+
+        private void readSetting()
+        {
+            if (File.Exists(setting_file_address))
+            {
+                StreamReader sr = new StreamReader(setting_file_address);
+                dynamic o = JsonConvert.DeserializeObject(sr.ReadToEnd());
+                sr.Close();
+
+                txtToken.Text = (string) o["last_token"];
+                txtUrlAddress.Text = (string) o["last_url"];
+                txtOffset.Text = (string) o["last_id"];
+                txtInterval.Text = (string) o["last_interval"];
+
+                //txtOffset.Text = (Int64.Parse(txtOffset.Text) + 1).ToString();
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            readSetting();
+        }
+
+        private void notifyIcon1_DoubleClick(object sender, EventArgs e)
+        {
+            if (hidden)
+            {
+                this.Show();
+                this.WindowState = FormWindowState.Normal;
+            }
+            else
+            {
+                this.Hide();
+            }
+            hidden = !hidden;
+
+        }
+
+        private void Form1_Resize(object sender, EventArgs e)
+        {
+            if (WindowState == FormWindowState.Minimized)
+            {
+                this.Hide();
+                hidden = !hidden;
+            }
+        }
+
+        private void contextMenuStrip1_ItemClicked(object sender, ToolStripItemClickedEventArgs e)
+        {
+            if (e.ClickedItem.Name == "show")
+                notifyIcon1_DoubleClick(null, null);
+            else
+                this.Hide();
+
+            hidden = !hidden;
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (timer1.Enabled)
+            {
+                MessageBox.Show("Fetching is in progress. Stop it before exit", "Warning",MessageBoxButtons.OK,MessageBoxIcon.Exclamation);
+                e.Cancel = true;
+            }
+        }
+
+        private void timReconnect_Tick(object sender, EventArgs e)
+        {
+            timer1_Tick(sender, e);
+        }
+
+        private void lblStatus_TextChanged(object sender, EventArgs e)
+        {
+            notifyIcon1.Text = "Status: " + lblStatus.Text;
+            notifyIcon1.ShowBalloonTip(1000, "Status" ,lblStatus.Text,ToolTipIcon.Info);
+        }
+    }
+}
